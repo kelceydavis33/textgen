8f270904eed8bbffd56cf934174533c92e791e03
Kelcey Davis <kelceyslandon@yahoo.com>
2020-07-02 12:46:47 -0700

    Added user input

--- textgen.py
+++ textgen.py
@@ -7,6 +7,8 @@ Created on Sun Jun 28 22:26:52 2020
 """
 
 #name, nominal_freq, actual_freq, resistance
+#change 2 inputs, where plots are and where to save
+#
 
 
 import os
@@ -16,18 +18,21 @@ import pandas as pd
 import numpy as np
 
 #Create a list of all file folders in current directory
-folders = os.listdir(os.curdir)  
-#Idendify the file path to the current directory
-working_directory = os.getcwd()
+input_folders = input("What is the absolute path to the folder containing the data files? ")#os.listdir(os.curdir)  
+#Get the names of all the folders
+folders = glob.glob(input_folders)
 #Create a directory to store the text files
 os.mkdir(working_directory +'/text_files')
 #Note the name of the directory to save the files
-text_file_directory = (working_directory + '/' + 'text_files/')
+text_file_directory = input("Where should the output text files be saved? ")#(working_directory + '/' + 'text_files/')
 
 #Look at each indiviual folder
 for folder in folders:
     #grab each data file within the directory
-	data_files = glob.glob(working_directory + '/' + folder +  '/' + 'IceCrate*_OUTPUT.pkl')
+	data_files = glob.glob(folders + '/' + folder +  '/' + 'IceCrate*_OUTPUT.pkl')
+    #Print the iteration in the list and clear the previous one so user can keep track
+    os.system('clear')
+    print("Generating text file {} of {}".format(folders.index(folder), len(folders))
 	#Loop over teh data files
     for fname in data_files:
         #Open the file
@@ -35,12 +40,15 @@ for folder in folders:
         #Load the pickle data
 		data = pkl.load(f)
         #Loop over the channels 
-		for chan in range( 1, len(data['subtargets'])))
+
+		for chan in range( 1, len(data['subtargets']))
             #Grab the relavent info
-			bolometer = data['subtargets'][chan]['bolometer']]
+			bolometer = data['subtargets'][chan]['bolometer']
 			nom_freq = data['subtargets'][chan]['frequency']
 			resist = data['overbiased'][chan]['R']
 			act_freq = data['overbiased'][chan]['freq']
+            
+            
             
             #Define the data
             pd_data = {'Name':[bolometer], 
@@ -56,3 +64,5 @@ for folder in folders:
             np.savetext(text_file_directory + '{}.txt'.format(folder), 
                         df.values, delimiter = "\t", 
                         header = "name\tnom_freq\tctfreq\tresist")
+            #Close the file 
+            f.close()
